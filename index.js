813c7c8acfe0b8bd8f695ea1af88e9f359cd0011
Di Wang <wangdicoder@gmail.com>
2016-12-10 03:14:23 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wangdi on 8/12/16.
+ */
+'use strict';
+
+import Pulse from './lib/loader/PulseLoader';
+import Circle from './lib/loader/CircleLoader';
+import Text from './lib/loader/TextLoader';
+
+export const CircleLoader = Circle;
+export const PulseLoader  = Pulse;
+export const TextLoader = Text;
