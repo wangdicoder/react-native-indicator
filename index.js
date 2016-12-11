f6bcecc6f002c0a8ce019b108b71650b11a7bc7c
Di Wang <wangdicoder@gmail.com>
2016-12-11 23:10:51 +1100

    MultipleDotsLoader

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
 import Pulse from './lib/loader/PulseLoader';
 import Circle from './lib/loader/CircleLoader';
 import Text from './lib/loader/TextLoader';
+import MultipleDots from './lib/loader/MultipleDotsLoader';
 
 export const CircleLoader = Circle;
 export const PulseLoader  = Pulse;
 export const TextLoader = Text;
+export const MultipleDotsLoader = MultipleDots;
